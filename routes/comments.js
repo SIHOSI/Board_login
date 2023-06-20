58fa432061a01dccd6747efcba22da91addd6e3d
Siho <siho8808@icloud.com>
2023-06-20 18:29:56 +0900

    댓글 수정 기능 구현, 확인

--- routes/comments.js
+++ routes/comments.js
@@ -51,12 +51,72 @@ router.post('/posts/:postId/comments', authMiddleware, async (req, res) => {
 router.get('/posts/:postId/comments', async (req, res) => {
   const { postId } = req.params;
   try {
+    if (!mongoose.isValidObjectId(postId)) {
+      return res.status(400).json({ errorMessage: '유효하지 않은 게시글ID' });
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(400).json({ errorMessage: '존재하지 않는 게시글ID' });
+    }
+
     const comments = await Comment.find({ postId }).sort({ syncTime: -1 });
+
     res.status(200).json({ all_Comments: comments });
   } catch (error) {
     console.error(error);
     res.status(500).json({ errorMessage: '서버 에러' });
   }
 });
+
+//댓글 수정
+router.patch(
+  '/posts/:postId/comments/:commentId',
+  authMiddleware,
+  async (req, res) => {
+    const { postId, commentId } = req.params;
+    const { commentcontent } = req.body;
+    const { user } = res.locals;
+
+    try {
+      if (!mongoose.isValidObjectId(postId)) {
+        return res.status(400).json({ errorMessage: '유효하지 않은 게시글ID' });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (!post) {
+        return res.status(400).json({ errorMessage: '존재하지 않는 게시글ID' });
+      }
+
+      if (!mongoose.isValidObjectId(commentId)) {
+        return res.status(400).json({ errorMessage: '유효하지 않은 댓글ID' });
+      }
+
+      const comment = await Comment.findById(commentId);
+
+      if (!comment) {
+        return res.status(400).json({ errorMessage: '존재하지 않는 댓글ID' });
+      }
+
+      if (!commentcontent) {
+        return res.status(400).json({ errorMessage: '댓글을 입력해주세요.' });
+      } else {
+        comment.commentcontent = commentcontent;
+        comment.syncTime = Date.now();
+        await comment.save();
+      }
+
+      res.status(201).json({
+        success: true,
+        comment: comment,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ errorMessage: '서버 에러' });
+    }
+  }
+);
 
 module.exports = router;
